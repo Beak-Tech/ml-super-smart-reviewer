7f08352f7597bb2db3c8e291dd54d6c35550e863
WhyTheMoon <107178692+WhyTheMoon@users.noreply.github.com>
2022-08-15 00:14:01 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import torch
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("roberta-large") 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+path='model_new.pt'
+
+model=torch.load(path)
+
+def predict(question, passage):
+    sequence = tokenizer.encode_plus(question, passage, return_tensors="pt")['input_ids'].to(device)
+    if(len(sequence[0]) > 512):
+        return 
+    logits = model(sequence)[0]
+    probabilities = torch.softmax(logits, dim=1).detach().cpu().tolist()[0]
+    proba_yes = round(probabilities[1], 2)
+    proba_no = round(probabilities[0], 2)
+    return [proba_yes, proba_no]
